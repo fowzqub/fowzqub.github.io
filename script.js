e4a39938aac7aa647a767c7e0becfc555118e39f
fowzqub <fowzqubgaming1@gmail.com>
2017-06-24 17:23:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,11 @@
-
-function setCookie(cname,cvalue,exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires=" + d.toGMTString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-
-function checkCookie() {
-    var user=getCookie("username");
-    if (user != "") {
-        alert("Welcome again " + user);
-    } else {
-       user = prompt("Please enter your name:","");
-       if (user != "" && user != null) {
-           setCookie("username", user, 30);
-       }
-    }
-}
-
+var index = new Array(8);
+var columns = document.getElementById("columns");
+index[0] = columns[0].childNodes;
+index[1] = columns[1].childNodes;
+index[2] = columns[2].childNodes;
+index[3] = columns[3].childNodes;
+index[4] = columns[4].childNodes;
+index[5] = columns[5].childNodes;
+index[6] = columns[6].childNodes;
+index[7] = columns[7].childNodes;
+index[5][5].style.backgroundColor = "red";
