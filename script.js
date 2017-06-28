13b976949132e32e12226a02e74f19a2b9c9ab22
fowzqub <fowzqubgaming1@gmail.com>
2017-06-27 18:47:29 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var index = new Array(8);
 var columns = document.getElementById("columns").childNodes;
+var piece = document.getElementById("wBishopOne");
 index[0] = columns[1].childNodes;
 index[1] = columns[3].childNodes;
 index[2] = columns[5].childNodes;
@@ -8,3 +9,8 @@ index[4] = columns[9].childNodes;
 index[5] = columns[11].childNodes;
 index[6] = columns[13].childNodes;
 index[7] = columns[15].childNodes;
+
+function move(var x, var y){
+  piece.style.left = x*90;
+  piece.style.top = y*90;
+}
