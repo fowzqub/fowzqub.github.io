154b0ecfa577fdce74ffc4df5fa57470f8dca6d1
fowzqub <fowzqubgaming1@gmail.com>
2017-06-28 13:54:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -10,15 +10,6 @@ index[5] = columns[11].childNodes;
 index[6] = columns[13].childNodes;
 index[7] = columns[15].childNodes;
 
-
-
- for(var m=0;m<8;m++) {
-    for(var n=1;n<15;n+=2) { 
-      var o = 0;
-      index[m][n].onclick = move(m,o);
-      o++;    
-    }
- }
 function move(x,y){
   piece.style.left = x*90+"px";
   piece.style.top = y*90+"px";
