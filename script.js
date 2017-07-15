1fdc8c9171bc941d70c64cd8740f144e95f382f4
fowzqub <fowzqubgaming1@gmail.com>
2017-07-15 00:49:26 -0700

    Update script.js

--- script.js
+++ script.js
@@ -35,6 +35,23 @@ var wp6 = new piece("pawn",5,1,document.getElementById("wPawnSix"));
 var wp7 = new piece("pawn",6,1,document.getElementById("wPawnSeven"));
 var wp8 = new piece("pawn",7,1,document.getElementById("wPawnEight"));
   
+var br1 = new piece("rook",0,0,document.getElementById("bCastleOne"));
+var bkn1 = new piece("knight",1,0,document.getElementById("bKnightOne"));
+var bb1 = new piece("bishop",2,0,document.getElementById("bBishopOne"));
+var bq = new piece("queen",3,0,document.getElementById("bCastleOne"));
+var bk = new piece("king",4,0,document.getElementById("bCastleOne"));
+var bb2 = new piece("bishop",5,0,document.getElementById("bBishopTwo"));
+var bkn2 = new piece("knight",6,0,document.getElementById("bKnightTwo"));
+var br2 = new piece("rook",7,0,document.getElementById("bCastleTwo"));
+var bp1 = new piece("pawn",0,1,document.getElementById("bPawnOne"));
+var bp2 = new piece("pawn",1,1,document.getElementById("bPawnTwo"));
+var bp3 = new piece("pawn",2,1,document.getElementById("bPawnThree"));
+var bp4 = new piece("pawn",3,1,document.getElementById("bPawnFour"));
+var bp5 = new piece("pawn",4,1,document.getElementById("bPawnFive"));
+var bp6 = new piece("pawn",5,1,document.getElementById("bPawnSix"));
+var bp7 = new piece("pawn",6,1,document.getElementById("bPawnSeven"));
+var bp8 = new piece("pawn",7,1,document.getElementById("bPawnEight"));
+
 function move(x,y){
   select.id.style.left = x*90+"px";
   select.id.style.top = y*90+"px";
